2263f9ca0c7f079ce6cb837bab96f12947db9927
amandat <amandatome@me.com>
2019-02-05 14:29:38 -0500

    jQuery
    Started to add jQuery functions.

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+//Load page
+//$(document).ready(function() {
+
+//When the game starts a random target number will be generated for the player
+
+//Generate random number for target
+        var targetNumber = Math.floor(Math.random() *(120-19+ 1)) + 19;
+        console.log(targetNumber);
+
+        //Assign target number to location
+
+//A random number will be assigned to the gems
+
+    //when the gem is clicked the number for that gem will be added to the players progress score * 4
+    var gem1 = Math.floor(Math.random() *(12-1+1)) + 1;
+    console.log(gem1);
+
+    var gem2 = Math.floor(Math.random() *(12-1+1)) + 1;
+    console.log(gem2);
+
+    var gem3 = Math.floor(Math.random() *(12-1+1)) + 1;
+    console.log(gem3);
+
+    var gem4 = Math.floor(Math.random() *(12-1+1)) + 1;
+    console.log(gem4);
+
+    //Assign to item
+
+//Stats will display on screen
+//If the player reaches target without going over then win++
+    //game reset
+//Else if the player goes over the target
+    //losses++
+    //game resets    
+
+
+
+
+
+
+
+
+
+
+
+
+
+//});
